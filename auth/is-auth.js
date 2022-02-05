7420debb90801c334b3d444fa9e859d1bf36692b
Knipl Gergely <knipl.gergely@greenformatics.hu>
2022-02-05 11:03:39 +0100

    authentication middleware checks jwt token

--- auth/is-auth.js
+++ auth/is-auth.js
@@ -1,9 +1,19 @@
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  if (ture) {
-    //jwt or auth
-    console.log('if true then false!!!')
-    return false
+  const token = req.headers['authorization']
+  if (!token) {
+    return res.status(403).send('token is required for authentication')
   }
 
-  next()
+  try {
+    console.log(token, { replaced: token.replace(/^Bearer\s+/, '') })
+    const replacedToken = token.replace(/^Bearer\s+/, '')
+    const decoded = jwt.verify(replacedToken, 'THISISMYTOKENKEY')
+    req.user = decoded
+    console.log({ decoded })
+  } catch {
+    return res.status(401).send('Invalid Token')
+  }
+  return next()
 }
